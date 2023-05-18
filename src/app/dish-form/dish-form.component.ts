527ecb8a45c09320c8fefeb526e74c88d1a038f0
anadragicevic <anaa.dragicevic97@gmail.com>
2023-05-18 16:49:41 +0200

    form-validation

--- src/app/dish-form/dish-form.component.ts
+++ src/app/dish-form/dish-form.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CategoriesService } from '../services/categories.service';
 import { subscribeOn } from 'rxjs';
 import { ProductService } from '../services/product.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-dish-form',
@@ -9,18 +10,23 @@ import { ProductService } from '../services/product.service';
   styleUrls: ['./dish-form.component.css']
 })
 export class DishFormComponent implements OnInit {
-  
+
   categories$;
 
-  constructor(private categoriesService:CategoriesService, private productService:ProductService) { 
-    this.categoriesService.getCategories().subscribe(categories=>this.categories$=categories);
+  constructor(private router:Router, private categoriesService: CategoriesService, private productService: ProductService) {
+    this.categoriesService.getCategories().subscribe(categories => this.categories$ = categories);
   }
 
   ngOnInit(): void {
   }
 
-  save(product){
-     this.productService.create(product);
+  save(product) {
+    this.productService.create(product);
+    this.router.navigateByUrl('/kreiranjeJelovnika');
+  }
+
+  delete(){
+
   }
 
 }
